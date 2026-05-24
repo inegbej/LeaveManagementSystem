820db63b5b2ee1b731822a7736a10db66b757071
Joseph Inegbe <joseph.inegbe@hotmail.com>
2026-05-25 00:07:58 +0100

    Asp.net core and entity framework core app for leave management system

---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
